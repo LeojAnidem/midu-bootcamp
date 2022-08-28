972c03e9f02fa06f97760810b7016fe923d0f52b
@LeojAnidem <jeremykaizer@gmail.com>
2022-08-28 01:30:18 -0400

    notes progresando

--- status-component/src/App.js
+++ status-component/src/App.js
@@ -1,21 +1,62 @@
-import { notes } from './db/notes';
+import { useState } from 'react'
 import { Note } from './modules/Note';
 import './styles.css';
 
-export const App = () => {
+export const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
   if (typeof notes === 'undefined' || notes.length === 0) {
     return <h1>No hay notas que mostrar!</h1>
   }
 
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const noteToAddToState = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+
+    setNotes([...notes, noteToAddToState])
+    setNewNote('')
+  }
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll)
+  }
+
   return (
-    <ol>
-      {
-        notes.map((note) => {
-          return (
-            <Note key={note.id} {...note} />
-          )
-        })
-      }
-    </ol>
+    <div>
+      <h1>Notes</h1>
+
+      <button onClick={handleShowAll}>
+        {showAll ? 'Show only important' : 'Show all'}
+      </button>
+
+      <ol>
+        {
+          notes
+            .filter(note => showAll ? true : note.important === true)
+            .map((note) => {
+              return (
+                <Note key={note.id} {...note} />
+              )
+            })
+        }
+      </ol>
+
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={newNote} />
+        <button>Crear notas</button>
+      </form>
+    </div>
   )
 }
--- status-component/src/index.js
+++ status-component/src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import {App} from './App'
+import { App } from './App'
+import { notes } from './db/notes'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <App notes={notes} />
 );
